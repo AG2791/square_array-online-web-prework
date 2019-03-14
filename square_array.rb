79f6fb6454abf9672ec579b1676ae2c75a2e6244
AJ Guiles <ashiguiles@gmail.com>
2019-03-14 02:13:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,10 @@
  def square_array(numbers)
     numbers=[1,2,3]
     numbers_squared = numbers.each{|x| x**2}
+<<<<<<< HEAD
     return numbers_squared 
+=======
+    
+>>>>>>> 73fac3b2cbe52c636a89cd11dfc40083a985ac08
  end 
   return square_array(numbers)
